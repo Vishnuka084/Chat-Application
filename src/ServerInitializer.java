7c32d88088acbd74596cfe89dadcbf5017888e34
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-08 01:36:39 +0530

    Updated Content

--- src/ServerInitializer.java
+++ src/ServerInitializer.java
@@ -1,5 +1,9 @@
-public class ServerInitializer {
-    public static void main(String[] args) {
+import javafx.application.Application;
 
+import static javafx.application.Application.launch;
+
+public class ServerInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
 }
