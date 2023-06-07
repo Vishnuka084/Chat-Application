f01124adaea58bd2f8fd1227161b21dd071d9ea1
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-08 01:54:53 +0530

    Updated Content

--- src/lk/ijse/play_tech/ChatApp/model/Client.java
+++ src/lk/ijse/play_tech/ChatApp/model/Client.java
@@ -1,6 +1,5 @@
 package lk.ijse.play_tech.ChatApp.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,32 +14,59 @@ import java.net.Socket;
 @Setter
 @Getter
 public class Client {
-    private int port;
     private String name;
+    private int port;
+    private ServerSocket serverSocket;
     private Socket accept;
     private Socket imgSocket;
-    private ServerSocket serverSocket;
-    private InputStream imgInputStream;
-    private OutputStream imgOutputStream;
     private DataInputStream dataInputStream;
     private DataOutputStream dataOutputStream;
+    private InputStream imgInputStream;
+    private OutputStream imgOutputStream;
     private String message = "";
-
-
 
     public Client(int port) {
         this.port = port;
     }
-
 
     public void acceptConnection() throws IOException {
         serverSocket = new ServerSocket(port);
         this.accept = serverSocket.accept();
     }
 
+    public void acceptImgConnection(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        this.imgSocket = serverSocket.accept();
+    }
 
     public void setInputAndOutput() throws IOException {
         this.dataInputStream = new DataInputStream(accept.getInputStream());
         this.dataOutputStream = new DataOutputStream(accept.getOutputStream());
+    }
+
+    public void setImageInputAndOutput() throws IOException {
+        this.imgInputStream = imgSocket.getInputStream();
+        this.imgOutputStream = imgSocket.getOutputStream();
+    }
+
+    public void processTextMessage() throws IOException {
+        while (!message.equals("exit")) {
+            message = "Client 1 : " + dataInputStream.readUTF();
+            System.out.println(message);
+            String typeName = dataInputStream.getClass().getTypeName();
+            System.out.println(typeName);
+
+            if (message.equals("Client 1 : exit")) {
+                accept = null;
+                return;
+            }
+        }
+    }
+
+    private void sendTextMessage(String message) throws IOException {
+        if (this.accept != null) {
+            this.dataOutputStream.writeUTF(message.trim());
+            this.dataOutputStream.flush();
+        }
     }
 }
