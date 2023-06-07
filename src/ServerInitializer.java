6ecc99f4678341e0f4e82b1774601acab86a9d76
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-08 01:39:38 +0530

    Fully setup serverInitializer class

--- src/ServerInitializer.java
+++ src/ServerInitializer.java
@@ -1,9 +1,19 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
 
 public class ServerInitializer extends Application {
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/ijse/play_tech/ChatApp/view/ServerForm.fxml"))));
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.show();
+
     }
 }
