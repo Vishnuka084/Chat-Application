537c5bfc3bc67a9f62bfe936228116fa93cdaa30
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-07 21:19:27 +0530

    Creat entity class

---
+++ src/lk/ijse/play_tech/ChatApp/model/Client.java
@@ -0,0 +1,14 @@
+package lk.ijse.play_tech.ChatApp.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Client {
+}
