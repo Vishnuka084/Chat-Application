ac663a419bb5b9d7b58baae6536d25a0dbb4344a
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-08 01:50:32 +0530

    Updated Content

---
+++ src/Client3_Initializer.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Client3_Initializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/ijse/play_tech/ChatApp/view/LoginForm3.fxml"))));
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.show();
+    }
+}
