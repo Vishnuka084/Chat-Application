6b4b4420f940264296878cfbdac50db897b5df90
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-07 21:23:00 +0530

    add attribute

--- src/lk/ijse/play_tech/ChatApp/model/Client.java
+++ src/lk/ijse/play_tech/ChatApp/model/Client.java
@@ -6,9 +6,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 public class Client {
+    private int port;
+    private String name;
+    private Socket accept;
+    private Socket imgSocket;
+    private ServerSocket serverSocket;
+    private InputStream imgInputStream;
+    private OutputStream imgOutputStream;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private String message = "";
+
+
+
+
 }
