01524254cef4011b124ca914f8f70a6374b8781d
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-06-07 20:53:18 +0530

    Updaetd content

--- src/lk/ijse/play_tech/ChatApp/controller/ServerFormController.java
+++ src/lk/ijse/play_tech/ChatApp/controller/ServerFormController.java
@@ -1,4 +1,26 @@
 package lk.ijse.play_tech.ChatApp.Controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ServerFormController {
+    public ScrollPane msgContext;
+    public Label lblClient;
+    public TextField txtMessage;
+    public AnchorPane emoji;
+
+    public void btnSendOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnEmojiOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnImageChooserOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnExitOnAction(MouseEvent mouseEvent) {
+    }
 }
